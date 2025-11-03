7da98e20c201df1afe22d3b88ce93403516c92fe
Rohit <rohitrajkumar.1810@gmail.com>
2025-11-04 01:27:01 +0530

    class graph

--- Phase-1/Graph.hpp
+++ Phase-1/Graph.hpp
@@ -1,8 +1,34 @@
-#include <vector>
+#include<bits/stdc++.h>
+using namespace std;
 
-class Graph
-{
-    int V; // number of vertices
+struct Node{
+    int node_id;
+    double lat;
+    double lon;
+    vector<string> pois;
+};
 
-    std::vector<std::vector<int>> adj; //adjacency list to store edges
+struct Edge{
+    int edge_id;
+    int node1;
+    int node2;
+    double len;
+    double avg_time;
+    vector<double> spd_profile;// will be empty if no spd profile
+    int type;// primary = 1, secondary = 2, tertiary = 3, local = 0, expressway = 4
+    bool oneway;// true if directed edge
+    bool disabled;
+};
+
+class Graph{
+    int num_nodes;
+    vector<Node> nodes;
+    map<int,Edge> edges;// edge_id -> edge
+    vector<vector<int>> adjlist;
+    //adjlist[node1_id][node2_id] = edge_id
+
+    Graph(){
+        
+    };
+
 };
